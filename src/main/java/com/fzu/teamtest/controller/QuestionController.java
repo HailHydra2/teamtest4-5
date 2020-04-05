0264cb4f4dba34b627bb4fd35f51a4700bbf6846
fzuwsh <1306321810@qq.com>
2020-04-05 22:49:35 +0800

    wsh3

--- src/main/java/com/fzu/teamtest/controller/QuestionController.java
+++ src/main/java/com/fzu/teamtest/controller/QuestionController.java
@@ -1,9 +1,12 @@
 package com.fzu.teamtest.controller;
 
 import com.fzu.teamtest.dao.QuestionDao;
+import com.fzu.teamtest.dao.ResponseDao;
+import com.fzu.teamtest.dao.UserDao;
 import com.fzu.teamtest.model.AjaxResponse;
 import com.fzu.teamtest.model.Question;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.ResultMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +20,30 @@ public class QuestionController {
     @Resource
     private QuestionDao questionDao;
 
-    @RequestMapping("/question")
+    @PostMapping("/question")
     public @ResponseBody AjaxResponse addQuestion(@RequestBody Question question){
         questionDao.insert(question);
         return AjaxResponse.success(question);
+
     }
 
-    @RequestMapping("/questions/{id}")
+    @GetMapping("/questions/{id}")
     public @ResponseBody AjaxResponse getQuestion(@PathVariable int id){
         return AjaxResponse.success(questionDao.selectByPrimaryKey(id));
     }
 
-    
+    @PutMapping("/questions/{id}")
+    public @ResponseBody AjaxResponse updateQuestion(@PathVariable int id, @RequestBody Question question){
+        question.setId(id);
+        questionDao.updateByPrimaryKey(question);
+        return AjaxResponse.success(question);
+    }
+
+    @DeleteMapping("/question/{id}")
+    public @ResponseBody AjaxResponse deleteQuestion(@PathVariable int id){
+        questionDao.deleteByPrimaryKey(id);
+        return AjaxResponse.success(id);
+    }
+
+
 }
